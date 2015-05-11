8682478dd8c45193f925ccc7917583936711b77c
Kevin Adler <kadler@us.ibm.com>
2015-05-11 13:21:08 -0500

    ibmi: re-disable test_196_InsertRetrieveXMLData_02 on IBM i
    Although DB2 for i 7.1+ support the XML datatype, it does not support
    the XMLQUERY function, so disabling this test for IBM i again.

--- IBM_DB/ibm_db/tests/test_196_InsertRetrieveXMLData_02.py
+++ IBM_DB/ibm_db/tests/test_196_InsertRetrieveXMLData_02.py
@@ -20,7 +20,11 @@ class IbmDbTestCase(unittest.TestCase):
     conn = ibm_db.connect(config.database, config.user, config.password)
     server = ibm_db.server_info( conn )
 
-    if (not self.obj.isServerInformix(server)):
+    if (self.obj.isServerInformix(server)):
+      print('Native XML datatype is not supported.')
+    elif self.obj.isServerIBMi(server):
+      print('XMLQUERY not supported')
+    else:
       try:
           rc = ibm_db.exec_immediate(conn, "DROP TABLE xml_test")
       except:
@@ -53,8 +57,6 @@ class IbmDbTestCase(unittest.TestCase):
       while( result ):
         print("Result from only XMLQuery: " + repr(result[0]))
         result = ibm_db.fetch_both(stmt)
-    else:
-      print('Native XML datatype is not supported.')
 
 #__END__
 #__LUW_EXPECTED__
@@ -70,10 +72,6 @@ class IbmDbTestCase(unittest.TestCase):
 #Result from XMLSerialize and XMLQuery: <zip>66061</zip>
 #Result from only XMLQuery: u?'%s<zip>66061</zip>'
 #__SYSTEMI_EXPECTED__
-#Result ID: 1
-#Result DATA: xml test 1
-#Result XMLCOL: u?'%s<address><street>12485 S Pine St.</street><city>Olathe</city><state>KS</state><zip>66061</zip></address>'
-#Result from XMLSerialize and XMLQuery: <zip>66061</zip>
-#Result from only XMLQuery: u?'%s<zip>66061</zip>'
+#XMLQUERY not supported
 #__IDS_EXPECTED__
 #Native XML datatype is not supported.
