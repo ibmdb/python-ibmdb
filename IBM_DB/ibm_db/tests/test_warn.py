43782145f209429b1efdaa7a82d151b9a3a6e443
hemlatabhatt <hemlbhat@in.ibm.com>
2016-02-09 14:49:20 +0530

    IBM_DB/ibm_db/tests/test_warn.py

--- IBM_DB/ibm_db/tests/test_warn.py
+++ IBM_DB/ibm_db/tests/test_warn.py
@@ -3,10 +3,6 @@
 #
 #  (c) Copyright IBM Corp. 2007-2016
 #
-import ibm_db
-import config
-from testfunctions import IbmDbTestFunctions
-
 class IbmDbTestCase(unittest.TestCase):
     def test_warn(self):
         obj = IbmDbTestFunctions()
@@ -14,11 +10,11 @@ class IbmDbTestCase(unittest.TestCase):
 
     def run_test_warn(self):
         conn = ibm_db.connect(config.database, config.user, config.password)
+        
         # Get the server type
         serverinfo = ibm_db.server_info( conn )
-
+        
         if conn:
-
             drop = "DROP TABLE WITH_CLOB"
             try:
                 result = ibm_db.exec_immediate(conn,drop)
@@ -27,22 +23,25 @@ class IbmDbTestCase(unittest.TestCase):
 
             # Create the table with_clob
             create = "CREATE TABLE WITH_CLOB (id SMALLINT NOT NULL, clob_col CLOB(1k))"
- 
             result = ibm_db.exec_immediate(conn, create)
+
             # Select the result from the table. This is just to verify we get appropriate warning using
             # ibm_db.stmt_warn() API
-
             query = 'SELECT * FROM WITH_CLOB'
             if (serverinfo.DBMS_NAME[0:3] != 'IDS'):
-               stmt = ibm_db.prepare(conn, query, {ibm_db.SQL_ATTR_CURSOR_TYPE: ibm_db.SQL_CURSOR_KEYSET_DRIVEN})
+                stmt = ibm_db.prepare(conn, query, {ibm_db.SQL_ATTR_CURSOR_TYPE: ibm_db.SQL_CURSOR_KEYSET_DRIVEN})
             else:
-                stmt = ibm_db.prepare(conn, query)
+               stmt = ibm_db.prepare(conn, query)
+
             ibm_db.execute(stmt)
+            print(ibm_db.stmt_warn(stmt))
+
             data = ibm_db.fetch_both( stmt )
             if data:
                 print("Success")
             else:
                 print("No Data")
+                print(ibm_db.stmt_warn(stmt))
             ibm_db.close(conn)
         else:
             print ("Connection failed.")
@@ -52,9 +51,10 @@ class IbmDbTestCase(unittest.TestCase):
 #[IBM][CLI Driver] CLI0005W  Option value changed. SQLSTATE=01S02 SQLCODE=-99999
 #No Data
 #__ZOS_EXPECTED__
-#[IBM][CLI Driver] CLI0005W  Option value changed. SQLSTATE=01S02 SQLCODE=-99999
-#No Data
+#[IBM][CLI Driver] CLI0005W  Option value changed. SQLSTATE=01S02 SQLCODE=-99999No Data[IBM][CLI Driver][DB2] SQL0100W  
+#No row was found for FETCH, UPDATE or DELETE; or the result of a query is an empty table.  SQLSTATE=02000 SQLCODE=100
 #__SYSTEMI_EXPECTED__
 #No Data
 #__IDS_EXPECTED__
 #No Data
+#[IBM][CLI Driver][IDS/UNIX64] SQL0100W  No row was found for FETCH, UPDATE or DELETE; or the result of a query is an empty table.  SQLSTATE=02000 SQLCODE=100
