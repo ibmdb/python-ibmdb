fd693a67e5f0b11195380f078c3b9981ef89c750
hemlatabhatt <hemlbhat@in.ibm.com>
2017-08-10 16:36:43 +0530

    ibmdb 2.8

---
+++ IBM_DB/ibm_db/test/test_061_Tables_02.py
@@ -0,0 +1,104 @@
+# 
+#  Licensed Materials - Property of IBM
+#
+#  (c) Copyright IBM Corp. 2007-2008
+#
+
+import unittest, sys
+import ibm_db
+import config
+from testfunctions import IbmDbTestFunctions
+
+class IbmDbTestCase(unittest.TestCase):
+
+  def test_061_Tables_02(self):
+    obj = IbmDbTestFunctions()
+    obj.assert_expectf(self.run_test_061)
+
+  def run_test_061(self):
+    conn = ibm_db.connect(config.database, config.user, config.password)
+    server = ibm_db.server_info( conn )
+
+    create = 'CREATE SCHEMA AUTHORIZATION t'
+    try:
+      result = ibm_db.exec_immediate(conn, create) 
+    except:
+      pass
+    
+    create = 'CREATE TABLE t.t1( c1 integer, c2 varchar(40))'
+    try:
+      result = ibm_db.exec_immediate(conn, create) 
+    except:
+      pass
+    
+    create = 'CREATE TABLE t.t2( c1 integer, c2 varchar(40))'
+    try:
+      result = ibm_db.exec_immediate(conn, create) 
+    except:
+      pass
+    
+    create = 'CREATE TABLE t.t3( c1 integer, c2 varchar(40))'
+    try:
+      result = ibm_db.exec_immediate(conn, create) 
+    except:
+      pass
+      
+    create = 'CREATE TABLE t.t4( c1 integer, c2 varchar(40))'
+    try:
+      result = ibm_db.exec_immediate(conn, create) 
+    except:
+      pass
+    
+    if conn:
+      server = ibm_db.server_info( conn )
+      if (server.DBMS_NAME[0:3] == 'IDS'):
+        op = {ibm_db.ATTR_CASE: ibm_db.CASE_UPPER}
+        ibm_db.set_option(conn, op, 1)
+
+      if (server.DBMS_NAME[0:3] == 'IDS'):
+        result = ibm_db.tables(conn, None, 't');
+      else:
+        result = ibm_db.tables(conn, None, 'T');    
+      i = 0
+      row = ibm_db.fetch_both(result)
+      while ( row ):
+        str = row['TABLE_SCHEM'] + row['TABLE_NAME'] + row['TABLE_TYPE']
+        if (i < 4):
+          print(str)
+        i = i + 1
+        row = ibm_db.fetch_both(result)
+
+      ibm_db.exec_immediate(conn, 'DROP TABLE t.t1')
+      ibm_db.exec_immediate(conn, 'DROP TABLE t.t2')
+      ibm_db.exec_immediate(conn, 'DROP TABLE t.t3')
+      ibm_db.exec_immediate(conn, 'DROP TABLE t.t4')
+
+      print("done!")
+    else:
+      print("no connection: %s" % ibm_db.conn_errormsg())
+
+#__END__
+#__LUW_EXPECTED__
+#TT1TABLE
+#TT2TABLE
+#TT3TABLE
+#TT4TABLE
+#done!
+#__ZOS_EXPECTED__
+#TT1TABLE
+#TT2TABLE
+#TT3TABLE
+#TT4TABLE
+#done!
+#__SYSTEMI_EXPECTED__
+#TT1TABLE
+#TT2TABLE
+#TT3TABLE
+#TT4TABLE
+#done!
+#__IDS_EXPECTED__
+#tt1TABLE%s
+#tt2TABLE%s
+#tt3TABLE%s
+#tt4TABLE%s
+#done!
---
+++ IBM_DB/ibm_db/test/test_231_FieldTypeName.py
@@ -0,0 +1,139 @@
+# 
+#  Licensed Materials - Property of IBM
+#
+#  (c) Copyright IBM Corp. 2007-2008
+#
+
+import unittest, sys
+import ibm_db
+import config
+from testfunctions import IbmDbTestFunctions
+
+class IbmDbTestCase(unittest.TestCase):
+
+  def test_231_FieldTypeName(self):
+    obj = IbmDbTestFunctions()
+    obj.assert_expect(self.run_test_231)
+
+  def run_test_231(self):
+    conn = ibm_db.connect(config.database, config.user, config.password)
+    
+    result = ibm_db.exec_immediate(conn, "select * from sales")
+    result2 = ibm_db.exec_immediate(conn, "select * from staff")
+    result3 = ibm_db.exec_immediate(conn, "select * from emp_photo")
+    
+    for i in range(0, ibm_db.num_fields(result) + 1):
+      print(str(i) + ":" + str(ibm_db.field_type(result,ibm_db.field_name(result,i))))
+    
+    print("\n-----")
+    
+    for i in range(0, ibm_db.num_fields(result2)):
+      print(str(i) + ":" + ibm_db.field_type(result2,ibm_db.field_name(result2,i)))
+    
+    print("\n-----")
+    
+    for i in range(0, 3):
+      print(str(i) + ":" + ibm_db.field_type(result3,ibm_db.field_name(result3,i)))
+    
+    print("\n-----")
+    
+    print("region:%s" % ibm_db.field_type(result,'region'))
+    print("5:%s" % ibm_db.field_type(result2,5))
+
+#__END__
+#__LUW_EXPECTED__
+#0:date
+#1:string
+#2:string
+#3:int
+#4:False
+#
+#-----
+#0:int
+#1:string
+#2:int
+#3:string
+#4:int
+#5:decimal
+#6:decimal
+#
+#-----
+#0:string
+#1:string
+#2:blob
+#
+#-----
+#region:False
+#5:decimal
+#__ZOS_EXPECTED__
+#0:date
+#1:string
+#2:string
+#3:int
+#4:False
+#
+#-----
+#0:int
+#1:string
+#2:int
+#3:string
+#4:int
+#5:decimal
+#6:decimal
+#
+#-----
+#0:string
+#1:string
+#2:blob
+#
+#-----
+#region:False
+#5:decimal
+#__SYSTEMI_EXPECTED__
+#0:date
+#1:string
+#2:string
+#3:int
+#4:False
+#
+#-----
+#0:int
+#1:string
+#2:int
+#3:string
+#4:int
+#5:decimal
+#6:decimal
+#
+#-----
+#0:string
+#1:string
+#2:blob
+#
+#-----
+#region:False
+#5:decimal
+#__IDS_EXPECTED__
+#0:date
+#1:string
+#2:string
+#3:int
+#4:False
+#
+#-----
+#0:int
+#1:string
+#2:int
+#3:string
+#4:int
+#5:decimal
+#6:decimal
+#
+#-----
+#0:string
+#1:string
+#2:blob
+#
+#-----
+#region:string
+#5:decimal
