44d494b91423d5988c17d744e8b6135b4bb02386
chsymann89 <christoph@symann.de>
2018-05-18 13:40:21 +0200

    removed broken check when adding M2M field
    The implementation brought up an Exception when adding a new M2M-field with selfwritten through-Model. That happened because auto_create is False in this case, it is a notnull-field so it was trying to alter the column to not null throwing "Column, attribute, or period [M2M-field-name] is not defined in [table-name]".
    All the logic that determines if it is an M2M that has to be auto/manually created is already done in the super().add_field call. A M2M-field never adds a new column but always a new table. It does not matter if it's auto/manually created. So all the notnull, unique and p_key stuff is never relevant.

--- IBM_DB/ibm_db_django/ibm_db_django/schemaEditor.py
+++ IBM_DB/ibm_db_django/ibm_db_django/schemaEditor.py
@@ -527,18 +527,8 @@ class DB2SchemaEditor(BaseDatabaseSchemaEditor):
         field._unique = False
         
         super(DB2SchemaEditor, self).add_field(model, field)
-        if( djangoVersion[0:2] < ( 1, 9 ) ):
-            if field.rel is not None and hasattr(field.rel,'through'):
-                rel_condition = field.rel.through._meta.auto_created
-            else:
-                rel_condition = False
-        else:
-            if field.remote_field is not None and hasattr(field.remote_field,'through'):
-                rel_condition = field.remote_field.through._meta.auto_created
-            else:
-                rel_condition = False
-                
-        if isinstance(field, ManyToManyField) and rel_condition:
+        
+        if isinstance(field, ManyToManyField):
             return
         else:
             self._reorg_tables()
