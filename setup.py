e748c33f0735c020b9cc23a3d557cdd90bc40274
Earammak <107038275+Earammak@users.noreply.github.com>
2023-03-30 11:31:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools.command.build_ext import build_ext
 from setuptools.command.install import install
 
 PACKAGE = 'ibm_db_wheel'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 LICENSE = 'Apache License 2.0'
 readme = os.path.join(os.path.dirname(__file__),'README.md')
 
