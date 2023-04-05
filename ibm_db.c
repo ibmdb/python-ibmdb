238421d884032ff322673c335dafa4048cc29de8
Earammak <107038275+Earammak@users.noreply.github.com>
2023-04-05 15:05:23 +0530

    Update ibm_db.c

--- ibm_db.c
+++ ibm_db.c
@@ -22,7 +22,7 @@
 +--------------------------------------------------------------------------+
 */
 
-#define MODULE_RELEASE "0.0.5"
+#define MODULE_RELEASE "0.0.6"
 
 #include <Python.h>
 #include <datetime.h>
