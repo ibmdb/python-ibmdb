d3c36f8c5d343f0ede1efc11ac5a1a6aadd0ffc8
Earammak <107038275+Earammak@users.noreply.github.com>
2023-04-20 15:29:59 +0530

    Update and rename ibm_db.py to ibm_db_wheel.py

--- ibm_db_wheel.py
+++ ibm_db_wheel.py
@@ -12,7 +12,7 @@ if sys.version_info >= (3,8,):
 def __bootstrap__():
     global __bootstrap__, __loader__, __file__
     import sys, pkg_resources, importlib.util
-    __file__ = pkg_resources.resource_filename(__name__, 'ibm_db_dlls\ibm_db.dll')
+    __file__ = pkg_resources.resource_filename(__name__, 'ibm_db.cp311-win_amd64.pyd')
     __loader__ = None; del __bootstrap__, __loader__
     spec = importlib.util.spec_from_file_location(__name__,__file__)
     mod = importlib.util.module_from_spec(spec)
