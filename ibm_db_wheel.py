0dff8e38657175e02038fc3a95d6ee8d4db2ac0c
Earammak <107038275+Earammak@users.noreply.github.com>
2023-04-20 15:34:40 +0530

    Update ibm_db_wheel.py

--- ibm_db_wheel.py
+++ ibm_db_wheel.py
@@ -10,13 +10,11 @@ if sys.version_info >= (3,8,):
         os.add_dll_directory(os.path.join(ibm_db_home, 'bin'))
 
 def __bootstrap__():
-    global __bootstrap__, __loader__, __file__
-    import sys, pkg_resources, importlib.util
-    __file__ = pkg_resources.resource_filename(__name__, 'ibm_db.cp311-win_amd64.pyd')
-    __loader__ = None; del __bootstrap__, __loader__
-    spec = importlib.util.spec_from_file_location(__name__,__file__)
-    mod = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(mod)
+   global __bootstrap__, __loader__, __file__
+   import sys, pkg_resources, imp
+   __file__ = pkg_resources.resource_filename(__name__,'ibm_db.cp311-win_amd64.pyd')
+   __loader__ = None; del __bootstrap__, __loader__
+   imp.load_dynamic(__name__,__file__)
 __bootstrap__()
 
 
