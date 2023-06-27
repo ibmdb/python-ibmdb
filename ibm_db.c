7a7e29345cc3b273600993bf8aaabcbc61ce5030
ek <ek@rocketsoftware.com>
2023-06-27 15:27:43 +0530

    test release 0.0.13

--- ibm_db.c
+++ ibm_db.c
@@ -22,7 +22,7 @@
 +--------------------------------------------------------------------------+
 */
 
-#define MODULE_RELEASE "0.0.12"
+#define MODULE_RELEASE "0.0.13"
 
 #include <Python.h>
 #include <datetime.h>
