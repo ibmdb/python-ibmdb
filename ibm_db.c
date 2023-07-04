bb6215d0babef1c89fcdcbd80c7b4cf07789215b
ek <ek@rocketsoftware.com>
2023-07-04 12:29:03 +0530

    change ibm_db version 3.1.4.1

--- ibm_db.c
+++ ibm_db.c
@@ -22,7 +22,7 @@
 +--------------------------------------------------------------------------+
 */
 
-#define MODULE_RELEASE "0.0.13"
+#define MODULE_RELEASE "3.1.4.1"
 
 #include <Python.h>
 #include <datetime.h>
