b11ec308752b34fe3152c3efd8b01581a40d8ed3
ek <ek@rocketsoftware.com>
2023-07-06 18:17:24 +0530

    update .gitignore and update ibm_db version 3.1.4.4

--- ibm_db.c
+++ ibm_db.c
@@ -22,7 +22,7 @@
 +--------------------------------------------------------------------------+
 */
 
-#define MODULE_RELEASE "3.1.4.3"
+#define MODULE_RELEASE "3.1.4.4"
 
 #include <Python.h>
 #include <datetime.h>
