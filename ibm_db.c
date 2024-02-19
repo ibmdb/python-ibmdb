89e5f29340170ab893a7d426ae994312809d4bad
bchoudhary6415 <95407446+bchoudhary6415@users.noreply.github.com>
2024-02-19 11:06:58 +0530

    Support for python3.12 (#913)
    * Support for python3.12
    
    Signed-off-by: Balram Choudhary <bchoudhary@rocketsoftware.com>

--- ibm_db.c
+++ ibm_db.c
@@ -1438,19 +1438,19 @@ static PyObject *_python_ibm_db_connect_helper( PyObject *self, PyObject *args, 
 #ifdef __MVS__
                 rc = SQLConnectW((SQLHDBC)conn_res->hdbc,
                     database,
-                    PyUnicode_GetSize(databaseObj)*2,
+                    PyUnicode_GetLength(databaseObj)*2,
                     uid,
-                    PyUnicode_GetSize(uidObj)*2,
+                    PyUnicode_GetLength(uidObj)*2,
                     password,
-                    PyUnicode_GetSize(passwordObj)*2);
+                    PyUnicode_GetLength(passwordObj)*2);
 #else
                 rc = SQLConnectW((SQLHDBC)conn_res->hdbc,
                     database,
-                    PyUnicode_GetSize(databaseObj),
+                    PyUnicode_GetLength(databaseObj),
                     uid,
-                    PyUnicode_GetSize(uidObj),
+                    PyUnicode_GetLength(uidObj),
                     password,
-                    PyUnicode_GetSize(passwordObj));
+                    PyUnicode_GetLength(passwordObj));
 #endif
                 Py_END_ALLOW_THREADS;
             }
@@ -1579,7 +1579,7 @@ static PyObject* getSQLWCharAsPyUnicodeObject(SQLWCHAR* sqlwcharData, int sqlwch
 
     if (maxuniValue <= 65536) {
     /* this is UCS2 python.. nothing to do really */
-        return PyUnicode_FromUnicode((Py_UNICODE *)sqlwcharData, sqlwcharBytesLen / sizeof(SQLWCHAR));
+        return PyUnicode_FromWideChar((Py_UNICODE *)sqlwcharData, sqlwcharBytesLen / sizeof(SQLWCHAR));
         }
 
     if (is_bigendian()) {
@@ -1619,7 +1619,7 @@ static SQLWCHAR* getUnicodeDataAsSQLWCHAR(PyObject *pyobj, int *isNewBuffer)
     long maxuniValue;
     PyObject *pyUTFobj;
     SQLWCHAR* pNewBuffer = NULL;
-    int nCharLen = PyUnicode_GET_SIZE(pyobj);
+    int nCharLen = PyUnicode_GET_LENGTH(pyobj);
 
     sysmodule = PyImport_ImportModule("sys");
     maxuni = PyObject_GetAttrString(sysmodule, "maxunicode");
@@ -1627,7 +1627,7 @@ static SQLWCHAR* getUnicodeDataAsSQLWCHAR(PyObject *pyobj, int *isNewBuffer)
 
     if (maxuniValue <= 65536) {
         *isNewBuffer = 0;
-        return (SQLWCHAR*)PyUnicode_AS_UNICODE(pyobj);
+        return (SQLWCHAR*)PyUnicode_AsWideCharString(pyobj,maxuniValue);
     }
 
     *isNewBuffer = 1;
@@ -5373,7 +5373,7 @@ static PyObject *_python_ibm_db_prepare_helper(conn_handle *conn_res, PyObject *
         if (PyString_Check(py_stmt) || PyUnicode_Check(py_stmt)) {
             py_stmt = PyUnicode_FromObject(py_stmt);
             if (py_stmt != NULL &&  py_stmt != Py_None) {
-                stmt_size = PyUnicode_GetSize(py_stmt);
+                stmt_size = PyUnicode_GetLength(py_stmt);
             } else {
                 PyErr_SetString(PyExc_Exception, "Error occure during processing of statement");
                 return NULL;
@@ -5893,7 +5893,7 @@ static int _python_ibm_db_bind_data( stmt_handle *stmt_res, param_node *curr, Py
                                     tmp_uvalue = NULL;
                                 }
                                 tmp_uvalue = getUnicodeDataAsSQLWCHAR(item, &isNewBuffer);
-                                curr->ivalue = PyUnicode_GetSize(item);
+                                curr->ivalue = PyUnicode_GetLength(item);
                                 curr->ivalue = curr->ivalue * sizeof(SQLWCHAR);
                             }
                             param_length = curr->ivalue;
@@ -6060,7 +6060,7 @@ static int _python_ibm_db_bind_data( stmt_handle *stmt_res, param_node *curr, Py
                             curr->uvalue = NULL;
                         }
                         curr->uvalue = getUnicodeDataAsSQLWCHAR(bind_data, &isNewBuffer);
-                        curr->ivalue = PyUnicode_GetSize(bind_data);
+                        curr->ivalue = PyUnicode_GetLength(bind_data);
                         curr->ivalue = curr->ivalue * sizeof(SQLWCHAR);
                     }
                     param_length = curr->ivalue;
